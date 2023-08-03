3aca10373ab107fffddc585759e40c41173f21c3
Scala Steward <scala_steward@virtuslab.com>
2023-08-03 22:02:56 +0000

    Reformat with scalafmt 3.7.12
    Executed command: scalafmt --non-interactive

--- shared/src/main/scala/stevedore/model.scala
+++ shared/src/main/scala/stevedore/model.scala
@@ -1,28 +1,27 @@
 package stevedore
 
-
-
-
-
 enum Command {
   case Build(build: stevedore.Build)
   case Run(hash: Hash)
 }
+
 case class Build(
   base: Build.Base,
-  commands: List[Build.Command]
+  commands: List[Build.Command],
 )
 
 object Build {
+
   enum Base {
     case EmptyImage
     case ImageReference(hash: Hash)
   }
+
   enum Command {
     case Upsert(key: String, value: String)
     case Delete(key: String)
   }
-  
+
   def empty = Build(Build.Base.EmptyImage, Nil)
 }
 
